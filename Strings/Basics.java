537a3399de3e49b60496b59ff64e5791c69852f4
tdilipkumar <vdilipkumar55op@gmail.com>
2024-04-05 20:38:37 +0530

    stringUpdated

---
+++ Strings/Basics.java
@@ -0,0 +1,18 @@
+ package Strings;
+
+class Basics {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Dilip"+5);
+        System.out.println("Dilip"+5+9);
+        System.out.println(5+9+"dilip");
+
+        //declaration
+        String str;
+        str = "dilip";
+        System.out.println(str);
+        str="vinay";
+        System.out.println(str+99);
+    }
+}
